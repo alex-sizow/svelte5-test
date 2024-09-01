256e23e2aa0347b21cc3482b18b8e61853f31dba
Alex Sizov <alex_sizow@outlook.com>
2024-09-01 14:34:53 +0300

    add filter and toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	import { text } from '@sveltejs/kit';
 
 	let todos = $state([
-		{ text: 'Eat bread!', done: true },
+		{ text: 'Eat bread!', done: false },
 		{ text: 'Drink milk!', done: false },
 		{ text: 'Not eat!!!!', done: false }
 	]);
+	let filter = $state('all');
+
 	const addTodo = (event) => {
 		if (event.key !== 'Enter') {
 			return;
@@ -16,18 +18,64 @@
 
 		const todo = { text: todoText, done: false };
 
-		todos = [todo, ...todos];
+		todos = [...todos, todo];
+
+		event.target.value = '';
 	};
+
+	const editTodo = (event) => {
+		const todoEl = event.target;
+		const index = todoEl.dataset.index;
+		todos[index].text = todoEl.value;
+	};
+
+	const toggleTodo = (event) => {
+		const todoEl = event.target;
+		const index = todoEl.dataset.index;
+		todos[index].done = !todos[index].done;
+	};
+
+	const setFilter = (type) => {
+		filter = type;
+	};
+
+	const filterTodos = () => {
+		switch (filter) {
+			case 'all':
+				return todos;
+			case 'done':
+				return todos.filter((todo) => todo.done);
+			case 'in work':
+				return todos.filter((todo) => todo.done === false);
+			default:
+				console.warn(`Unknown filter type: ${filter}`);
+				return todos;
+		}
+	};
+
+	let filteredTodos = $derived(filterTodos());
 </script>
 
 <ul class="todos">
+	{#each filteredTodos as todo, i}
+		<li class="todos__item">
+			<input oninput={editTodo} data-index={i} value={todo.text} type="text" />
+			<input
+				checked={todo.done}
+				onchange={toggleTodo}
+				data-index={i}
+				value={todo.done}
+				type="checkbox"
+			/>
+		</li>
+	{/each}
 	<li>
 		<input onkeydown={addTodo} class="todos__item" type="text" placeholder="Add todo..." />
 	</li>
-	{#each todos as todo, i}
-		<li class="todos__item">
-			<input value={todo.text} type="text" />
-			<input value={todo.done} type="checkbox" />
-		</li>
-	{/each}
+
+	<div class="todos__filter">
+		<button onclick={() => setFilter('all')} class="todos__filter_button">All</button>
+		<button onclick={() => setFilter('done')} class="todos__filter_button">Done</button>
+		<button onclick={() => setFilter('in work')} class="todos__filter_button">In work</button>
+	</div>
 </ul>
