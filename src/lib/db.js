786dd6ec33121156fb0248ec9bd2bdca3ae39656
Alex Sizov <alex_sizow@outlook.com>
2024-09-29 19:28:42 +0300

    add header

---
+++ src/lib/db.js
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+export const query = (text, params) => pool.query(text, params);
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<header>
+	<h1><a href="/">Webduck</a></h1>
+	<nav>
+		<a href="/shop">Shopping cart</a>
+		<a href="/todo">Todo</a>
+	</nav>
+</header>
+
+<main>
+	<slot />
+</main>
+
+<style scoped>
+	header {
+		width: 100%;
+		min-height: 50px;
+		background-color: black;
+		padding: 0 13px;
+		border-radius: 0 0 30px 30px;
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		h1 {
+			color: yellow;
+			font-size: 20px;
+		}
+
+		nav {
+			a {
+				margin-left: 20px;
+			}
+		}
+	}
+</style>
