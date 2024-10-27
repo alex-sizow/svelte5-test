53c60688f44ffeb498d888bfbee0149753cacd30
Alex Sizov <alex_sizow@outlook.com>
2024-10-27 21:49:33 +0300

    layout fixies

--- src/routes/temperature-converter/+page.svelte
+++ src/routes/temperature-converter/+page.svelte
@@ -5,15 +5,16 @@
 
 <h1>Temperature converter</h1>
 <section>
-	<input type="number" bind:value={c} /> Celsius =
-	<input type="number" bind:value={f} /> Fahrenheit
+	<input type="number" bind:value={c} /> Celsius <span>=</span>
+	<input type="number" bind:value={f} />Fahrenheit
 </section>
 
 <style scoped>
 	h1 {
 		display: flex;
 		justify-content: center;
-		font-size: 34px;
+		font-size: 44px;
+		font-weight: 700;
 		color: white;
 		margin-top: 100px;
 		margin-bottom: 20px;
@@ -21,15 +22,22 @@
 	section {
 		display: flex;
 		justify-content: center;
+		align-items: center;
 		gap: 20px;
 		font-size: 34px;
 		color: white;
+
+		span {
+			padding-top: 1px;
+			font-size: 50px;
+		}
 		> input[type='number'] {
 			color: black;
+			font-size: 34px;
 			max-width: 100px;
 			background-color: bisque;
 			border-radius: 10px;
-			padding: 5px;
+			padding: 10px;
 			font-size: 24px;
 		}
 	}
